a30678335e96629a024c5cebbb75e36d264947c9
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-05-07 14:50:11 +0300

    feat: add gulp sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
-const gulp = require('gulp'),
+const {src, dest, watch} = require('gulp'),
       browserSync = require('browser-sync').create(),
-      cleanCSS = require('gulp-clean-css');
-
-gulp.task('hello', (done) => {
-console.log('Привет, мир!')
-done();
-});
+      sass = require('gulp-sass');
 
 // новый плагин автоматическое обновление html страницы 
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
-//новый плагин clean css
-gulp.task('min', () => {
-  return gulp.src('./css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('demo/css'));
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./*.js").on('change', browserSync.reload);
+};
+
+// Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
