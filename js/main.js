b7846d718618876e3fa12b931cba5f34ffe27988
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-05-16 22:56:41 +0300

    feat: add btnUp to main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
   
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
+      closeBtn = $('.modal__close'),
+      btnUp = $('.scroll-up');
+
 
   modalBtn.on('click', function () {
     modal.toggleClass('modal--visible');
@@ -16,4 +18,16 @@ $(document).ready(function () {
       modal.toggleClass('modal--visible');
     }
 });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 500) {
+      btnUp.toggleClass('scroll-up--show');
+    } else {
+      btnUp.removeClass('scroll-up--show');
+    }
+  });
+  btnUp.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '500');
+  });
 });
