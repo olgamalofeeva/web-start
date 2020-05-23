844082488bea9587a524f3fd3c570ca80d860ada
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-05-23 22:45:42 +0300

    refact: add api yandex map

--- js/main.js
+++ js/main.js
@@ -145,4 +145,39 @@ $(document).ready(function () {
 //маска для номера телефона
 
 $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
+//создаем yandex карту
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.732692, 37.562344],
+          zoom: 9
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Наш офис',
+          balloonContent: 'Вход со двора'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/location.png',
+          // Размеры метки.
+          iconImageSize: [32, 32],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      });
+
+  myMap.geoObjects
+      .add(myPlacemark);
+});
 });
