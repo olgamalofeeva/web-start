ed5e9c33357d0c0a3c29f2a336d45fadd2543f24
Olga Malofeeva <oli.malofeeva@mail.ru>
2020-05-26 23:47:44 +0300

    refact

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const {src, dest, watch} = require('gulp'),
+const {src, dest, watch, series} = require('gulp'),
       browserSync = require('browser-sync').create(),
       sass = require('gulp-sass'),
-      autoprefixer = require('gulp-autoprefixer');
+      autoprefixer = require('gulp-autoprefixer'),
+      cleanCSS = require('gulp-clean-css'),
+      minify = require('gulp-minify'),
+      htmlmin = require('gulp-htmlmin'),
+      tinypng = require('gulp-tinypng-compress');
 
 // новый плагин автоматическое обновление html страницы 
 function bs() {
@@ -28,4 +32,62 @@ function serveSass() {
       .pipe(browserSync.stream());
 };
 
+function buildCSS(done) {
+  src('css/**/**.css')
+  .pipe(cleanCSS({compatibility: 'ie8'}))
+  .pipe(dest('dist/css'));
+  done();
+}
+
+function buildJS(done) {
+  src(['js/**.js', '!js/**.min.js'])
+    .pipe(minify({
+      ext:{
+          min:'.js'
+      },
+      exclude: ['tasks']}))
+    .pipe(dest('dist/js/'));
+    src('js/**.min.js').pipe(dest('dist/js/'));
+  done()
+}
+
+function html(done) {
+  src('**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+  done()
+}
+
+function php(done) {
+  src('**.php')
+    .pipe(dest('dist/'));
+  src('phpmailer/**/**')
+    .pipe(dest('dist/phpmailer/'));
+  done()
+}
+
+function fonts(done) {
+  src('fonts/**/**')
+    .pipe(dest('dist/fonts/'));
+  done()
+}
+
+function imagemin(done) {
+  src('img/**/**')
+    .pipe(tinypng({
+      key: 'HQdSgqf132jzDq0N84M7FRHtKxt5rQDF'
+  }))
+    .pipe(dest('dist/img/'));
+  src('img/types/**')
+    .pipe(tinypng({
+      key: 'HQdSgqf132jzDq0N84M7FRHtKxt5rQDF'
+  }))
+    .pipe(dest('dist/img/types/'));
+  src('img/**/*.svg')
+    .pipe(dest('dist/img/'));
+  src('img/**/*.svg')
+    .pipe(dest('dist/img/'));
+  done()
+}
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin)
